ad39c65e7b0b0bf9f28cf7a9b15d1bdbafab4846
Kristofer Walters <kris@wltrs.org>
2013-03-01 01:23:01 +0100

    grunt connect + static spec runner page

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,13 @@ module.exports = function(grunt) {
         vendor: ['lib/**/*.js', 'spec/lib/**/*.js']
       }
     },
+    connect: {
+      server: {
+        options: {
+          keepalive: true
+        }
+      }
+    },
     jshint: {
       options: {
         jshintrc: '.jshintrc'
@@ -75,6 +82,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
