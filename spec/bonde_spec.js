a38c9e3d904434981500901ced09c5b02835d026
Kristofer Walters <kris@wltrs.org>
2013-03-02 00:23:45 +0100

    Bonde module registry and initializer

--- spec/bonde_spec.js
+++ spec/bonde_spec.js
@@ -1,7 +1,211 @@
 /*global Bonde:false */
 
 describe('Bonde', function () {
-    it('should be defined', function () {
+
+    var customMatchers = {
+        toBeFunction: function () {
+            return typeof this.actual === 'function';
+        }
+    };
+
+    beforeEach(function () {
+        this.addMatchers(customMatchers);
+    });
+
+
+
+    it('Bonde should be defined', function () {
         expect( Bonde ).toBeDefined();
+    });
+
+
+
+    describe('Bonde.applyModule', function () {
+        beforeEach(function () {
+            this.callback = jasmine.createSpy();
+            this.el = DOMBuilder.createElement('div');
+
+            Bonde.registerModule('myModule', this.callback);
+        });
+
+        it("calls a registered module's callback", function () {
+            Bonde.applyModule('myModule', this.el);
+            expect( this.callback ).toHaveBeenCalled();
+        });
+
+        it("calls a registered module's callback", function () {
+            Bonde.applyModule('NOT-myModule', this.el);
+            expect( this.callback ).not.toHaveBeenCalled();
+        });
+    });
+
+
+
+    describe('Bonde.reset', function () {
+        it('unregisters modules', function () {
+            var callback = jasmine.createSpy();
+            var el = DOMBuilder.createElement('div');
+
+            Bonde.registerModule('myModule', callback);
+
+            Bonde.reset();
+
+            Bonde.applyModule('myModule', el);
+
+            expect( callback ).not.toHaveBeenCalled();
+        });
+    });
+
+
+
+    describe('Bonde.ModuleContext', function () {
+        beforeEach(function () {
+            this.el = DOMBuilder.build(['div', {'data-foo': 'bar'},
+                ['i', 'inner']
+            ]);
+
+            this.moduleContext = new Bonde.ModuleContext( this.el );
+        });
+
+        it("has el", function () {
+            expect( this.moduleContext.el ).toBe( this.el );
+        });
+
+        it("takes options from data attributes", function () {
+            expect( this.moduleContext.options ).toBeDefined();
+            expect( this.moduleContext.options['foo'] ).toEqual('bar');
+        });
+
+        it("has jQuery node $el", function () {
+            expect( this.moduleContext.$el ).toBeDefined();
+            expect( this.moduleContext.$el.jquery ).toBeDefined();
+        });
+
+
+        it("has a local selector finder", function () {
+            expect( this.moduleContext.$ ).toBeFunction();
+            expect( this.moduleContext.$(':first-child')[0] ).toBeDefined();
+            expect( this.moduleContext.$(':first-child')[0].outerHTML ).toEqual('<i>inner</i>');
+        });
+    });
+
+
+
+    describe('Bonde.ModuleContext node attachment', function () {
+        beforeEach(function () {
+
+            this.fooNode = DOMBuilder.createElement('span', {'data-attach-to': 'foo'}, 'bar');
+            this.fizNode = DOMBuilder.createElement('span', {'data-attach-to': 'fiz'}, 'baz');
+            this.el      = DOMBuilder.createElement('div', {}, [this.fooNode, this.fizNode]);
+
+            this.moduleContext = new Bonde.ModuleContext( this.el );
+        });
+
+        it("has refernce to dom node", function () {
+            expect( this.moduleContext.foo ).toBeDefined();
+            expect( this.moduleContext.foo ).toEqual( this.fooNode );
+        });
+
+        it("has refernce to jQueriefied node", function () {
+            expect( this.moduleContext.$foo ).toBeDefined();
+            expect( this.moduleContext.$foo.jquery ).toBeDefined();
+            expect( this.moduleContext.$foo.get(0) ).toEqual( this.fooNode );
+        });
+
+        describe('Attribute mapping', function () {
+            it('has attributes', function () {
+                expect( this.moduleContext.attr ).toBeDefined();
+            });
+
+            it('has getter', function () {
+                expect( this.moduleContext.attr.get ).toBeDefined();
+            });
+
+            it('gets node content', function () {
+                expect( this.moduleContext.attr.get('foo') ).toEqual('bar');
+            });
+
+            it('has setter', function () {
+                expect( this.moduleContext.attr.set ).toBeDefined();
+            });
+
+            it('has event thingie', function () {
+                expect( this.moduleContext.attr.on ).toBeDefined();
+            });
+
+            describe('changing value', function () {
+                beforeEach(function () {
+                    this.callback = jasmine.createSpy();
+                    this.moduleContext.attr.on('change', this.callback);
+                    this.moduleContext.attr.set('foo', 'any string');
+                });
+
+                it('changes getter value', function () {
+                    expect( this.moduleContext.attr.get('foo') ).toEqual('any string');
+                });
+
+                it('changes node content', function () {
+                    expect( this.fooNode.innerHTML ).toEqual('any string');
+                });
+
+
+                it('does not change other getter value', function () {
+                    expect( this.moduleContext.attr.get('fiz') ).not.toEqual('any string');
+                });
+
+                it('does not change other node content', function () {
+                    expect( this.fizNode.innerHTML ).not.toEqual('any string');
+                });
+
+
+                it('triggers event listener', function () {
+                    expect( this.callback ).toHaveBeenCalledWith('foo', 'any string', 'bar');
+                });
+            });
+
+            describe('without mapping to dom node', function () {
+                beforeEach(function () {
+                    this.callback = jasmine.createSpy();
+                    this.moduleContext.attr.on('change', this.callback);
+                });
+
+                it('is undefined by default', function () {
+                    expect( this.moduleContext.attr.get('ftw') ).toBeUndefined();
+                });
+
+                it('is changeable', function () {
+                    this.moduleContext.attr.set('ftw', 'any string');
+                    expect( this.moduleContext.attr.get('ftw') ).toEqual('any string');
+                });
+
+                it('trigger event listener', function () {
+                    this.moduleContext.attr.set('ftw', 'any string');
+                    expect( this.callback ).toHaveBeenCalledWith('ftw', 'any string', undefined);
+                });
+            });
+        });
+    });
+
+
+
+    describe('Bonde.scanForModules', function () {
+        it('applies module callback to all module dom nodes', function () {
+            var nodeA  = DOMBuilder.createElement('div', {'data-module': 'modA'});
+            var nodeB1 = DOMBuilder.createElement('div', {'data-module': 'modB'});
+            var nodeB2 = DOMBuilder.createElement('div', {'data-module': 'modB'});
+
+            var domTree = DOMBuilder.createElement('div', {}, [nodeA, nodeB1, nodeB2]);
+
+            spyOn(Bonde, 'applyModule');
+
+            Bonde.scanForModules( domTree );
+
+            expect( Bonde.applyModule ).toHaveBeenCalled();
+            expect( Bonde.applyModule.callCount ).toEqual( 3 );
+
+            expect( Bonde.applyModule ).toHaveBeenCalledWith('modA', nodeA);
+            expect( Bonde.applyModule ).toHaveBeenCalledWith('modB', nodeB1);
+            expect( Bonde.applyModule ).toHaveBeenCalledWith('modB', nodeB2);
+        });
     });
 });
